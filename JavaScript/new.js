d6352cdcded3ff3db06c21490aeb574b0105ccae
flowersoq <weiyunsyu@gmail.com>
2021-05-27 00:40:45 +0800

    fix JScode to Window.load

--- JavaScript/new.js
+++ JavaScript/new.js
@@ -2,12 +2,6 @@ const beverageList = ['greentee', 'straberry', 'berry'];
 const imgList = ['img/img1.png', 'img/img2.png', 'img/img3.png'];
 const colorList = ['green', 'pink', 'purple'];
 
-for(let i = 0; i < beverageList.length; i++) {
-    beverageList[i]
-    const tee = document.getElementById(beverageList[i]);
-    this.addEvent(tee, imgList[i], colorList[i]);
-}
-
 function addEvent(eleId, imgPath, color) {
     eleId.addEventListener('mouseenter', function(){
         document.querySelector('.starbucks').setAttribute('src', imgPath);
@@ -19,8 +13,14 @@ function addEvent(eleId, imgPath, color) {
     });
 } //非按鈕 : 滑動寫法 連動背景色(缺點:較不靈活)
 
-
-// window.addEventListener('load', init)
+function init(){
+    for(let i = 0; i < beverageList.length; i++) {
+        beverageList[i]
+        const tee = document.getElementById(beverageList[i]);
+        this.addEvent(tee, imgList[i], colorList[i]);
+    }
+}
+window.addEventListener('load', init)
 
 // function init(){
 //     document.querySelector('.All_img').querySelectorAll('img')
@@ -34,7 +34,6 @@ function addEvent(eleId, imgPath, color) {
 //     let new_img = e.target.getAttribute('src');
 //     document.querySelector('#imgbox').querySelector('img').setAttribute('src', new_img);
 // } //上課練習寫法 不改變背景色
-
 
 // document.querySelector('.greentee').addEventListener('click', function(){
 //     console.log("test "+(document.querySelector('.greentee').classList.length));
